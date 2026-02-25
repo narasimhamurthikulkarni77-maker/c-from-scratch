e2e8689a3d8577653e4f836936d2b0f501455422
Narasimhamurthi kulkarni <narasimhamurthikulkarni77@gmail.com>
2026-02-25 20:33:33 +0530

    new floder for solution 1

---
+++ solution1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int currentyear;
+    int age;
+  
+    scanf("%d", &currentyear);
+    scanf("%d", &age);
+    printf("i am born in year %d \n", currentyear-age);
+
+    return 0;
+
+}
